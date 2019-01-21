756f004c2164afe695d8d92a1405141bdd7926f5
Jean <jdeyehe@gmail.com>
2019-01-21 23:56:52 +0100

    Dev sign in

--- app/src/main/java/com/juanito/friendlystalk/MainActivity.java
+++ app/src/main/java/com/juanito/friendlystalk/MainActivity.java
@@ -1,13 +1,62 @@
 package com.juanito.friendlystalk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        final GoogleSignInClient mGoogleSignInClient =  GoogleSignIn.getClient(this, gso);
+
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account != null) {
+            Toast.makeText(this, "welcome " + account.getDisplayName(), Toast.LENGTH_SHORT).show();
+            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == REQUEST_CODE) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            GoogleSignInAccount account = task.getResult();
+            if (account != null) {
+                Toast.makeText(this, "Thanks " + account.getDisplayName(), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
