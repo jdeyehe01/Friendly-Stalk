5ec5fc3a604620055af2ffa5af23c033a60c9c9f
NealKemissi <neal.k@hotmail.fr>
2019-02-06 22:25:56 +0100

    implementation messagingService Firebase

--- app/src/main/java/com/juanito/friendlystalk/Services/MyFirebaseMessagingService.java
+++ app/src/main/java/com/juanito/friendlystalk/Services/MyFirebaseMessagingService.java
@@ -26,6 +26,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private void sendNotification(String body) {
 
         Intent intent = new Intent(this,LoginActivity.class);
+
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,PendingIntent.FLAG_ONE_SHOT);
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
