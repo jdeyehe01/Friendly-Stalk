4fec7f88f72b687e5f42305d5edfe2be124e2a3c
Jean <jdeyehe@gmail.com>
2019-02-12 01:35:52 +0100

    dev home page

--- app/src/main/java/com/juanito/friendlystalk/UserRepository.java
+++ app/src/main/java/com/juanito/friendlystalk/UserRepository.java
@@ -1,10 +1,9 @@
 package com.juanito.friendlystalk;
 
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
-import android.util.Log;
 
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -13,129 +12,99 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class UserRepository implements IUserDao {
 
-    private DatabaseReference database ;
-    private String id;
-    private List<User> res ;
+
+    private DatabaseReference database = FirebaseDatabase.getInstance().getReference("User");
+   // private String id = database.push().getKey();
+    private ArrayList<User> res ;
+
 
     public UserRepository(){
-       //FirebaseDatabase.getInstance().setPersistenceEnabled(true);
-       database = FirebaseDatabase.getInstance().getReference("User");
-       id = database.push().getKey();
-       res = new ArrayList<User>();
+
+        res = new ArrayList<User>();
     }
-
-
-
 
     @Override
     public void insert(User user) {
-        user.setId(id);
-        database.child(id).setValue(user);
+      /*  user.setId(id);
+        database.child(id).setValue(user);        */
+
+      new InsertUserAsync().execute(user);
     }
 
     @Override
-    public List<User> getAll() {
+    public ArrayList<User> getAll() {
         database.addListenerForSingleValueEvent(valueEventListener);
 
+        if(res.isEmpty()){
+            return null;
+        }
         return res;
     }
 
-    public User getTest(String t){
-        Query query = FirebaseDatabase.getInstance().getReference("User").orderByChild("email").equalTo(t);
+
+    @Override
+    public User getUserByPseudo(String pseudo) {
+        Query query = database.orderByChild("pseudo").equalTo(pseudo);
         query.addListenerForSingleValueEvent(valueEventListener);
         if(res.isEmpty()){
             return null;
         }
         return res.get(0);
     }
+    
+    @Override
+    public ArrayList<User> getFriends() {
+        
+        Query query = database.orderByChild("email").equalTo(FirebaseAuth.getInstance().getCurrentUser().getEmail());
+        Query friends = query.orderByChild("friends");
+        friends.addValueEventListener(valueEventListenerList);
+
+        if(res.isEmpty()){
+            return null;
+        }
+        return res;
+    }
 
     @Override
-    public User getUserByPseudo(String pseudo) {
+    public User findByEmail(String email) {
+        Query query = database.orderByChild("email").equalTo(email);
+        query.addListenerForSingleValueEvent(valueEventListener);
 
-        res.clear();
-
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("User");
-
-        Query query = ref.orderByChild("pseudo").equalTo(pseudo);
-
-        query.addListenerForSingleValueEvent(new ValueEventListener() {
-
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                if(dataSnapshot.exists()){
-
-                    for(DataSnapshot snapshot : dataSnapshot.getChildren()){
-                        User u = snapshot.getValue(User.class);
-                        if(u != null){
-                            res.add(u);
-                            break;
-                        }
-                    }
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-            }
-        });
         if(res.isEmpty()){
             return null;
         }
         return res.get(0);
     }
 
-    @Override
-    public List<User> getListFriendOneUser(String pseudoUser) {
-
-        res.clear();
 
 
-        List<String> pseudos = new ArrayList<String>();
+    static class InsertUserAsync extends AsyncTask<User, Void, Void>{
+        private DatabaseReference database;
+        private String id;
 
+        public InsertUserAsync()
+        {
+            this.database = FirebaseDatabase.getInstance().getReference("User");
 
+        }
 
-        return null;
-    }
+        @Override
+        protected Void doInBackground(User... users) {
+            for(User user : users)
+            {
+                this.id = this.database.push().getKey();
+                user.setId(id);
+                database.child(id).setValue(user);
 
-    @Override
-    public User findByEmail(String email) {
-
-        res.clear();
-
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("User");
-
-        Query query = ref.orderByChild("email").equalTo(email);
-
-        query.addListenerForSingleValueEvent(new ValueEventListener() {
-
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                if(dataSnapshot.exists()){
-
-                    for(DataSnapshot snapshot : dataSnapshot.getChildren()){
-                        User u = snapshot.getValue(User.class);
-                        if(u != null){
-                            res.add(u);
-                            break;
-                        }
-                    }
-                }
             }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-            }
-        });
-        if(res.isEmpty()){
             return null;
         }
-        return res.get(0);    }
+    }
+
 
 
         ValueEventListener valueEventListener = new ValueEventListener() {
@@ -156,4 +125,22 @@ public class UserRepository implements IUserDao {
 
             }
         };
+    
+    
+    ValueEventListener valueEventListenerList = new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            res.clear();
+
+            if (dataSnapshot.exists()) {
+                res = dataSnapshot.getValue(ArrayList.class);
+            }
+
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    };
 }
